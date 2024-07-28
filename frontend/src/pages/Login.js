e23208c481a593ba3438046588395f3ee90bcf7d
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-07-28 17:50:59 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
         }
 
         try {
-            const response = await fetch('http://localhost:3008/auth/login', {
+            const response = await fetch('https://deploy-mern-app-delta.vercel.app/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
