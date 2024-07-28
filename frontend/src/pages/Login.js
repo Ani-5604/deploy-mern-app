8c4d1378660006e09577fc5c18e385934cf8e54b
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-07-28 20:25:29 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
         }
 
         try {
-            const response = await fetch('https://deploy-mern-app-pi.vercel.app/auth/login', {
+            const response = await fetch('https://deploy-mern-app-eight.vercel.app/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
